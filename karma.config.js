64eb3d24c4c92bdc22c5cfe25ad4d9941f541497
chylli-binary <chylli@binary.com>
2021-08-29 15:33:40 +0000

    try kamra

---
+++ karma.config.js
@@ -0,0 +1,30 @@
+module.exports = function (config) {
+    config.set({
+        plugins: [
+            'karma-webpack',
+            'karma-jasmine',
+            'karma-chrome-launcher',
+        ],
+        // I'm starting a headless browser, but I can also swap this out for "Chrome" to add debug statements,
+        // inspect console logs etc.
+        browsers: ['ChromeHeadless'],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
+
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine'],
+
+        // list of files / patterns to load in the browser
+        // Here I'm including all of the the Jest tests which are all under the __tests__ directory.
+        // You may need to tweak this patter to find your test files/
+        files: ['./karma-setup.js', 'src/**/__tests__/**/*.js'],
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            // Use webpack to bundle our tests files
+            'src/**/__tests__/**/*.ts': ['webpack'],
+        },
+    });
+};
