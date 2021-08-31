b0f90cb1e4b9f2208a3095fc285b3b4311a3e28b
chylli-binary <chylli@binary.com>
2021-08-31 06:31:26 +0000

    tmp commit, marma setup

--- karma.config.js
+++ karma.config.js
@@ -1,3 +1,8 @@
+// from https://mike-ward.net/2015/09/07/tips-on-setting-up-karma-testing-with-webpack/
+// TODO since we have 2 entrypoints in this repo, we should test the first one first.
+// and maybe create another one in another files.
+var webpackConfig = require('./webpack.config.js');
+webpackConfig.entry = {};
 module.exports = function (config) {
     config.set({
         plugins: [
@@ -23,8 +28,10 @@ module.exports = function (config) {
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
+            './karma-setup.js': ['webpack'],
             // Use webpack to bundle our tests files
-            'src/**/__tests__/**/*.ts': ['webpack'],
+            'src/**/__tests__/**/*.js': ['webpack'],
         },
+        webpack: webpackConfig,
     });
 };
