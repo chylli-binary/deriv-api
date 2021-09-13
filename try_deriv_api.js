7a1b0623e8326ca56894d8d5e85848f38446d279
chylli-binary <chylli@binary.com>
2021-09-13 11:34:49 +0800

    sold is ok

--- try_deriv_api.js
+++ try_deriv_api.js
@@ -32,47 +32,76 @@ async function main() {
         const ticks = await api.ticks('frxUSDJPY');
         const ticks_subscription = ticks.onUpdate().subscribe((data) => {console.log("tick ")});
 
-        const contract = await api.contract({
+//        let contract = await api.contract({
+//            contract_type: 'CALL',
+//            currency,
+//            amount: 10,
+//            duration: 15,
+//            duration_unit: 'm',
+//            symbol: 'frxUSDJPY',
+//            basis: 'stake',
+//        });
+//
+//        //contract.onUpdate(console.log, console.log) ;
+//        contract.onUpdate(({ status, payout, bid_price }) => {
+//            switch (status) {
+//                case 'proposal':
+//                    return console.log(
+//                        `Current payout: ${payout.currency} ${payout.display}`);
+//                case 'open':
+//                    return console.log(
+//                        `Current bid price: ${bid_price.currency} ${bid_price.display}`);
+//                default:
+//                    console.log("contract onUpdate default");
+//                    break;
+//            };
+//        });
+//
+//        // Wait until payout is greater than USD 19
+//        await contract.onUpdate().pipe(find(({ payout }) => payout.value >= expected_payout)).toPromise();
+//        const buy = await contract.buy();
+//
+//        console.log(`Buy price is: ${buy.price.currency} ${buy.price.display}`);
+//        ticks_subscription.unsubscribe();
+//        // Wait until the contract is sold
+//        const timeout1 = timeout(5000);
+//        await Promise.any([contract.onUpdate().pipe(find(({ is_sold }) => is_sold)).toPromise(),
+//            timeout1.promise.then(() => console.log('not sold in 5 seconds'))]
+//        ).then(() => clearTimeout(timeout1.timeout_id));
+
+
+        let contract = await api.contract({
             contract_type: 'CALL',
             currency,
             amount: 10,
             duration: 15,
-            duration_unit: 'm',
-            symbol: 'frxUSDJPY',
+            duration_unit: 's',
+            symbol: 'R_100',
             basis: 'stake',
         });
 
-        //contract.onUpdate(console.log, console.log) ;
-        contract.onUpdate(({ status, payout, bid_price }) => {
-            console.log("comming..... contract");
-            switch (status) {
-                case 'proposal':
-                    return console.log(
-                        `Current payout: ${payout.currency} ${payout.display}`);
-                case 'open':
-                    return console.log(
-                        `Current bid price: ${bid_price.currency} ${bid_price.display}`);
-                default:
-                    console.log("contract onUpdate default");
-                    break;
-            };
-        });
-
-        // Wait until payout is greater than USD 19
-        await contract.onUpdate().pipe(find(({ payout }) => payout.value >= expected_payout)).toPromise();
+        await contract.onUpdate().pipe(first()).toPromise;
         const buy = await contract.buy();
+        console.log(`Buy price of second R_100 is: ${buy.price.currency} ${buy.price.display}`);
 
-        console.log(`Buy price is: ${buy.price.currency} ${buy.price.display}`);
-        ticks_subscription.unsubscribe();
+        const timeout2 = timeout(20000000);
         // Wait until the contract is sold
-        await Promise.any([contract.onUpdate().pipe(find(({ is_sold }) => is_sold)).toPromise(),
-            timeout(5000).then(() => console.log('not sold in 5 seconds'))]
-        );
+        await Promise.any([contract.onUpdate().pipe(find((data) => {console.log("---------------------------");return data.is_sold; })).toPromise(),
+            
+            timeout2.promise.then(() => console.log('not sold in 200 seconds'))
+        ]
+        ).then(() => {
+            let timeout_id = timeout2.timeout_id;
+            console.log(`timeout id ${timeout_id}`);
+            clearTimeout(timeout2.timeout_id);
+            console.log("clearing timeout2");
+        });
+        // console.log(contract);
+        const { profit, status, is_sold } = contract;
 
-        const { profit, status } = contract;
-
-        console.log(`You ${status}: ${profit.currency} ${profit.display}`);
-
+        if (is_sold) {
+            console.log(`You ${status}: ${profit.currency} ${profit.display}`);
+        }
 
     } catch (err) {
         console.error(err);
@@ -80,10 +109,13 @@ async function main() {
         // Close the connection and exit
         api.basic.disconnect();
     }
-    //await new Promise((resolve) => setTimeout(() => {console.log("end"); resolve('1')}, 5000));
+    // await new Promise((resolve) => setTimeout(() => {console.log("end"); resolve('1')}, 5000));
 
 }
 function timeout(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+    let timeout_id;
+    let promise = new Promise(resolve => {timeout_id = setTimeout(resolve, ms)});
+    console.log(`timeout id created ${timeout_id}`);
+    return { timeout_id, promise };
 }
 main();
